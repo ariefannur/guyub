23e00cea0b0d7364769a263b97f0eee95f14d71a
Arief maffrudin <arief.platk@gmail.com>
2023-12-11 15:00:14 +0700

    update readme

--- guyub/src/commonMain/kotlin/com/github/arief/annuur/guyub/utils/FakeData.kt
+++ guyub/src/commonMain/kotlin/com/github/arief/annuur/guyub/utils/FakeData.kt
@@ -176,7 +176,7 @@ object FakeData {
                 ),
                 title = "Notificatoin"
             ),
-                ItemBasic(
+            ItemBasic(
                 userField = UserField(
                     username = "jhondoe",
                     imgAvatar = "https://images.unsplash.com/profile-1441298803695-accd94000cac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=32085a077889586df88bfbe406692202",
