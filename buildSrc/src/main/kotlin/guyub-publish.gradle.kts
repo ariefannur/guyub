6f88d33d7fe34f702708b6c706bd0a454f0f0fc2
Arief maffrudin <arief.platk@gmail.com>
2023-12-12 23:31:36 +0700

    Add publish to maven central

---
+++ buildSrc/src/main/kotlin/guyub-publish.gradle.kts
@@ -0,0 +1,105 @@
+plugins {
+    `maven-publish`
+    signing
+}
+
+ext["signing.keyId"] = null
+ext["signing.password"] = null
+ext["signing.secretKey"] = null
+ext["signing.secretKeyRingFile"] = null
+ext["ossrhUsername"] = null
+ext["ossrhPassword"] = null
+ext["gradle.publish.key"] = null
+ext["gradle.publish.secret"] = null
+ext["user.id"] = null
+ext["user.name"] = null
+ext["user.email"] = null
+val localPropsFile = project.rootProject.file("local.properties")
+if (localPropsFile.exists()) {
+    localPropsFile.reader()
+        .use { java.util.Properties().apply { load(it) } }
+        .onEach { (name, value) ->
+            ext[name.toString()] = value
+        }
+} else {
+    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
+    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
+    ext["signing.secretKey"] = System.getenv("SIGNING_SECRET_KEY")
+    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
+    ext["ossrhUsername"] = System.getenv("OSSRH_USERNAME")
+    ext["ossrhPassword"] = System.getenv("OSSRH_PASSWORD")
+    ext["gradle.publish.key"] = System.getenv("GRADLE_PUBLISH_KEY")
+    ext["gradle.publish.secret"] = System.getenv("GRADLE_PUBLISH_SECRET")
+    ext["user.id"] = System.getenv("POM_USER_ID")
+    ext["user.name"] = System.getenv("POM_USER_NAME")
+    ext["user.email"] = System.getenv("POM_USER_EMAIL")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
+val signPublications = getExtraString("signing.keyId") != null
+
+publishing {
+    repositories {
+        maven {
+            name = "sonatype"
+            setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+
+        maven {
+            name = "SonatypeSnapshot"
+            setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        if (plugins.hasPlugin("org.jetbrains.kotlin.multiplatform")) {
+            artifact(tasks.register("${name}JavadocJar", Jar::class) {
+                archiveClassifier.set("javadoc")
+                archiveAppendix.set(this@withType.name)
+            })
+        }
+        if (signPublications) signing.sign(this)
+
+        groupId = "io.github.ariefannur"
+        version = "1.0.0-Alpha-01"
+
+        pom {
+            name.set("guyub")
+            description.set("Serverside rendering libraray with kotlin multi platform")
+            url.set("https://github.com/ariefannur/guyub")
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set(getExtraString("user.id"))
+                    name.set(getExtraString("user.name"))
+                    email.set(getExtraString("user.email"))
+                }
+            }
+            scm {
+                url.set("https://github.com/ariefannur/guyub")
+            }
+        }
+    }
+}
+
+if (signPublications) {
+    signing {
+        getExtraString("signing.secretKey")?.let { secretKey ->
+//            useInMemoryPgpKeys(getExtraString("signing.keyId"), secretKey, getExtraString("signing.password"))
+        }
+    }
+}
