1a8d38d60216e27967366b972c71acaf185e44ca
Arief maffrudin <arief.platk@gmail.com>
2024-01-15 14:00:06 +0700

    Update Swift Package Manager

---
+++ Sources/guyub/guyub.swift
@@ -0,0 +1,6 @@
+public struct guyub {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/guyubTests/guyubTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import guyub
+
+final class guyubTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(guyub().text, "Hello, World!")
+    }
+}
